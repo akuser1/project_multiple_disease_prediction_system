e0a617f0180922256a8852262f1259241b9b37e7
akuser1 <124675410+akuser1@users.noreply.github.com>
2023-07-16 03:13:02 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -1,41 +1,37 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jul 16 00:28:16 2023
+Created on Wed Jul  5 16:13:59 2023
 
 @author: Kaushik
 """
+
+
 import pickle
 import streamlit as st
-import pandas as pd
-import sklearn.datasets
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
 from streamlit_option_menu import option_menu
-from sklearn.metrics import accuracy_score
-
 
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('C:/Users/Kaushik/Desktop/multiple-disease prediction mini project sem 6/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:/Users/Kaushik/Desktop/multiple-disease prediction mini project sem 6/heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
 
-parkinsons_model = pickle.load(open('C:/Users/Kaushik/Desktop/multiple-disease prediction mini project sem 6/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 
- 
 
-# Sidebar for navigation
-
+# sidebar for navigation
 with st.sidebar:
+    
     selected = option_menu('Multiple Disease Prediction System',
-                          ['Diabetes Prediction','Breast Cancer Prediction', 'Heart Disease Prediction', 
-                           'Stroke Prediction','Parkinsons Prediction'],
-                          icons=['activity', 'globe','heart','clock','person'],
-                          default_index=0) 
-
-
+                          
+                          ['Diabetes Prediction',
+                           'Heart Disease Prediction',
+                           'Parkinsons Prediction'],
+                          icons=['activity','heart','person'],
+                          default_index=0)
+    
     
 # Diabetes Prediction Page
 if (selected == 'Diabetes Prediction'):
@@ -100,10 +96,9 @@ if (selected == 'Heart Disease Prediction'):
     
     with col1:
         age = st.text_input('Age')
-    age = int(age) if age.isdigit() else 0    
+        
     with col2:
         sex = st.text_input('Sex')
-    sex = int(sex) if sex.isdigit() else 0    
         
     with col3:
         cp = st.text_input('Chest Pain types')
@@ -147,10 +142,8 @@ if (selected == 'Heart Disease Prediction'):
     # creating a button for Prediction
     
     if st.button('Heart Disease Test Result'):
-        input_data = [[age, sex, int(cp), int(trestbps), int(chol), int(fbs), int(restecg), int(thalach), int(exang), float(oldpeak), int(slope), int(ca), int(thal)]]
-
-        heart_prediction = heart_disease_model.predict(input_data)
-
+        heart_prediction = heart_disease_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg,thalach,exang,oldpeak,slope,ca,thal]])                          
+        
         if (heart_prediction[0] == 1):
           heart_diagnosis = 'The person is having heart disease'
         else:
@@ -161,163 +154,6 @@ if (selected == 'Heart Disease Prediction'):
     
     
 
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-# Breast Cancer Prediction System Page
-if (selected == "Breast Cancer Prediction"):
-       
-    # Title of the application
-    st.title('Breast Cancer Prediction')
-    
-    # Data Collection & Processing
-    breast_cancer_dataset = sklearn.datasets.load_breast_cancer()
-    data_frame = pd.DataFrame(breast_cancer_dataset.data, columns=breast_cancer_dataset.feature_names)
-    data_frame['label'] = breast_cancer_dataset.target
-    
-    # Separating the features and target
-    X = data_frame.drop(columns='label', axis=1)
-    Y = data_frame['label']
-    
-    # Splitting the data into training data & testing data
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
-    
-    # Model Training
-    model = LogisticRegression()
-    model.fit(X_train, Y_train)
-    
-    # User input for prediction
-    st.header('Enter Input for Prediction')
-    input_data = []
-    for feature_name in breast_cancer_dataset.feature_names:
-        value = st.number_input(f'Enter value for {feature_name}', key=feature_name)
-        input_data.append(value)
-    
-    # Submit button
-    submit_button = st.button('Submit')
-    # code for Prediction
-    breast_diagnosis = ''
-    # Prediction Result
-    if submit_button:
-        # Predicting the result
-        prediction = model.predict([input_data])
-    
-    
-        # Displaying the result
-        st.header('Prediction Result')
-        if prediction[0] == 0:
-            breast_diagnosis = "The Breast cancer is Malignant"
-        else:
-            breast_diagnosis = "The Breast Cancer is Benign"
-          
-    st.success(breast_diagnosis)
-
-
-
-
-
-
-
-
-if (selected == "Stroke Prediction"):
-
-    # Create the Streamlit app
-    st.title('Stroke Prediction System')
-    
-    
-    # Load the dataset
-    data = pd.read_csv('C:/Users/Kaushik/Desktop/multiple-disease prediction mini project sem 6/healthcare-dataset-stroke-data.csv')
-    
-    
-    # code for Prediction
-    stroke_diagnosis = ''
-    
-    
-    # Select features and target variable
-    features = data[['age', 'gender', 'hypertension', 'heart_disease', 'ever_married', 'work_type', 'Residence_type',
-                     'avg_glucose_level', 'bmi', 'smoking_status']]
-    target = data['stroke']
-    
-    # Convert categorical variables to numerical using one-hot encoding
-    features_encoded = pd.get_dummies(features, drop_first=True)
-    
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(features_encoded, target, test_size=0.2, random_state=42)
-    
-    # Train the Logistic Regression model
-    model = LogisticRegression()
-    model.fit(X_train, y_train)
-    
-   
-    
-    # Collect user input
-    age = st.slider('Age', 0, 100)
-    gender = st.selectbox('Gender', ['Male', 'Female'])
-    hypertension = st.checkbox('Hypertension')
-    heart_disease = st.checkbox('Heart Disease')
-    ever_married = st.selectbox('Marital Status', ['Yes', 'No'])
-    work_type = st.selectbox('Work Type', ['Private', 'Self-employed', 'Govt_job', 'children', 'Never_worked'])
-    residence_type = st.selectbox('Residence Type', ['Urban', 'Rural'])
-    glucose_level = st.number_input('Average Glucose Level')
-    bmi = st.number_input('BMI')
-    smoking_status = st.selectbox('Smoking Status', ['formerly smoked', 'never smoked', 'smokes', 'Unknown'])
-    
-    # Create a button for prediction
-    predict_button = st.button('Predict')
-    
-    # Make predictions based on user input when the button is clicked
-    if predict_button:
-        # Prepare input data for prediction
-        input_data = pd.DataFrame({
-            'age': [age],
-            'gender': [gender],
-            'hypertension': [hypertension],
-            'heart_disease': [heart_disease],
-            'ever_married': [ever_married],
-            'work_type': [work_type],
-            'Residence_type': [residence_type],
-            'avg_glucose_level': [glucose_level],
-            'bmi': [bmi],
-            'smoking_status': [smoking_status]
-        })
-    
-        # Convert categorical variables to numerical using one-hot encoding
-        input_data_encoded = pd.get_dummies(input_data, drop_first=True)
-    
-        # Align input data with training data columns
-        input_data_aligned = input_data_encoded.reindex(columns=X_train.columns, fill_value=0)
-    
-        # Predict the result
-        prediction = model.predict(input_data_aligned)
-        
-        
-    
-        # Display the prediction result
-        st.header('Prediction Result')
-        if prediction[0] == 1:
-            stroke_diagnosis = "The individual is predicted to have a stroke."
-        else:
-            stroke_diagnosis = "The individual is predicted to not have a stroke."
-      
-    st.success(stroke_diagnosis)
-    
-    
-    
-    
-    
-    
-    
-    
-    
 # Parkinson's Prediction Page
 if (selected == "Parkinsons Prediction"):
     
@@ -407,4 +243,5 @@ if (selected == "Parkinsons Prediction"):
           parkinsons_diagnosis = "The person does not have Parkinson's disease"
         
     st.success(parkinsons_diagnosis)
-        
+
+
